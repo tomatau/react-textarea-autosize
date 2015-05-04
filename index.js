52ed48f6daad406e6ee3ed9512737edc56a55d12
Andrey Popp <8mayday@gmail.com>
2015-05-05 00:59:28 +0500

    Docs

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var React = require('react');
+var TextareaAutosize = require('../src/TextareaAutosize');
+
+var Demo = React.createClass({
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Component with rows set</h2>
+          <pre>
+{`
+  <TextareaAutosize
+    rows={4}
+    defaultValue="Just a single line..."
+    />
+`}
+          </pre>
+          <TextareaAutosize
+            rows={4}
+            defaultValue="Just a single line..."
+            />
+        </div>
+        <div>
+          <h2>Controlled mode</h2>
+          <pre>
+{`
+  <TextareaAutosize
+    useCacheForDOMMeasurements
+    value={this.state.value}
+    onChange={e => this.setState({value: e.target.value})}
+    />
+`}
+          </pre>
+          <TextareaAutosize
+            useCacheForDOMMeasurements
+            value={this.state.value}
+            onChange={e => this.setState({value: e.target.value})}
+            />
+          <button onClick={this.changeValueProgramatically}>
+            Change value programatically
+          </button>
+        </div>
+        <div>
+          <h2>Uncontrolled mode</h2>
+          <pre>
+{`
+  <TextareaAutosize
+    defaultValue={this.state.value}
+    />
+`}
+          </pre>
+          <TextareaAutosize
+            defaultValue={this.state.value}
+            />
+        </div>
+      </div>
+    );
+  },
+
+  getInitialState() {
+    var value = (new Array(15)).join('\nLine.');
+    return {value};
+  },
+
+  changeValueProgramatically() {
+    var value = 'This value was set programatically';
+    this.setState({value});
+  }
+});
+
+React.render(<Demo />, document.getElementById('main'));
